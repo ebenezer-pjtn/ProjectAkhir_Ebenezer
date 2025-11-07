3fc93bfcc203e677a043002e7c6758821da84203
ebenezer-pjtn <kelas22s03@gmail.com>
2025-11-07 09:13:10 +0700

    Second commit - Upload Project Akhir Ebenezer

--- app/src/main/java/com/example/projectakhir_ebenezer/MainActivity.java
+++ app/src/main/java/com/example/projectakhir_ebenezer/MainActivity.java
@@ -1,24 +1,63 @@
 package com.example.projectakhir_ebenezer;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+import com.example.projectakhir_ebenezer.db.DbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Deklarasi variabel
+    DbHelper dbHelper;
+    private EditText inNama, inNIK;
+    private Button btnSimpan, btnList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); // Layout utama
+
+        // Inisialisasi database helper
+        dbHelper = new DbHelper(this);
+
+        // Hubungkan variabel dengan komponen di layout XML
+        inNIK = findViewById(R.id.inp_nik);
+        inNama = findViewById(R.id.inp_nama);
+        btnSimpan = findViewById(R.id.btn_submit);
+        btnList = findViewById(R.id.btn_list);
+
+        // Tombol SIMPAN ditekan
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nik = inNIK.getText().toString().trim();
+                String nama = inNama.getText().toString().trim();
+
+                if (nik.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Error: NIK harus diisi!", Toast.LENGTH_SHORT).show();
+                } else if (nama.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Error: Nama Lengkap harus diisi!", Toast.LENGTH_SHORT).show();
+                } else {
+                    dbHelper.addUserDetail(nik, nama);
+                    inNIK.setText("");
+                    inNama.setText("");
+                    Toast.makeText(MainActivity.this, "Simpan berhasil!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Tombol DAFTAR MASYARAKAT ditekan
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, com.example.projectakhir_ebenezer.ListMasyarakatActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
